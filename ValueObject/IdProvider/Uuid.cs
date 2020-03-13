eb5a5768881d00c7b2bfd7424de76c6d6c2507d3
あると <mail@cfm-art.net>
2020-03-13 15:54:39 +0900

    🚧：uuidを自作へ変更.。

--- ValueObject/IdProvider/Uuid.cs
+++ ValueObject/IdProvider/Uuid.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Runtime.InteropServices;
+using CfmArt.Uuid;
 
 namespace CfmArt.ValueObject.IdProvider
 {
@@ -8,32 +8,11 @@ namespace CfmArt.ValueObject.IdProvider
     /// </summary>
     internal static class Uuid
     {
-        /// <summary>
-        /// Windows APIでuuidv1を取得
-        /// </summary>
-        /// <param name="uuid"></param>
-        /// <returns></returns>
-        [DllImport("Rpcrt4.dll", SetLastError = true)]
-        private extern static int UuidCreateSequential(out Guid uuid);
-
-        /// <summary>
-        /// UUIDv1の作成
-        /// </summary>
-        public static Guid NewUuidV1()
-        {
-            const int RPC_S_OK = 0;
-            int hresult = UuidCreateSequential(out Guid result);
-
-            if (hresult != RPC_S_OK)
-            {
-                throw new ApplicationException("UuidCreateSequential is failed: " + hresult);
-            }
-
-            return result;
-        }
+        /// <summary>UUIDv1の作成</summary>
+        public static Guid NewUuidV1() => Uuid.NewUuidV1();
 
         /// <summary>UUIDv4相当を生成</summary>
-        public static Guid NewUuidV4() => Guid.NewGuid();
+        public static Guid NewUuidV4() => Uuid.NewUuidV4();
 
         /// <summary>UUIDv1相当を生成</summary>
         public static Guid NewUuid() => NewUuidV1();
